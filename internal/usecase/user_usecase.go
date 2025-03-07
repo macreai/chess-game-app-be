72d75c3796454292731073f6f124d667ad9ac3f6
arda <arda@ardas-Mac-mini.local>
2025-03-07 21:25:12 +0700

    add redis (fix)

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -143,7 +143,7 @@ func (c *UserUseCase) Login(request *model.LoginUserRequest) *model.WebResponse[
 		}
 	}
 
-	c.RedisDB.Set(ctxBackground, fmt.Sprintf("%d", user.ID), token, time.Until(exp))
+	c.RedisDB.Set(ctxBackground, fmt.Sprintf("%d", user.ID), token, time.Until(exp)) // check
 
 	return &model.WebResponse[*model.LoginUserResponse]{
 		Errors: nil,
